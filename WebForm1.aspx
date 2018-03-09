b042148264a5f668ee5e1f925deb533ae153f72c
ruipedrovnc <37220814+ruipedrovnc@users.noreply.github.com>
2018-03-09 16:42:21 +0000

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>    
+    <%--<link rel="stylesheet" href="css/bootstrap.min.css" />--%>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <div class="row">
+                <div class="center-block" style="width: 300px; background-color: #ccc; margin: auto;margin-top:10%;padding:15px;text-align:center;">
+                    <div class="input-group">
+                        <asp:TextBox runat="server" id="TextBoxPesquisa" name="Pesquisa" CssClass="form-control" placeholder="Pesquisar..." type="Text"/>
+                        <div class="input-group-btn">
+                            <asp:LinkButton runat="server" OnClick="ButtonPesquisa_Click"  class="btn btn-default"><i class="glyphicon glyphicon-search"></i></asp:LinkButton>
+                        </div>
+                    </div>
+                    <%--Labels--%>
+                    <div>
+                        Erros: <asp:Label ID="LabelErros" runat="server" Height="20px" Width="100px"></asp:Label>
+                        <br />
+                        Registos: <asp:Label ID="LabelRegistos" runat="server"></asp:Label>
+                        <br />
+                        Coluna: <asp:Label ID="LabelColuna" runat="server"></asp:Label>
+                        <br />
+                        IP: <asp:Label ID="LabelIP" runat="server"></asp:Label>
+                    </div>
+                    <%--Repeater--%>
+                    <div>
+                        <asp:Repeater ID="Repeater1" runat="server" OnItemCommand="Repeater1_ItemCommand">
+                            <HeaderTemplate>
+                                <table border="1">
+                                    <tr>
+                                        <td><b>IXP</b></td>
+                                        <td><b>IXS1</b></td>
+                                    </tr>
+                            </HeaderTemplate>
+                            <ItemTemplate>
+                                <tr>
+                                    <td><%# DataBinder.Eval(Container.DataItem, "ixp") %> </td>
+                                    <td><%# DataBinder.Eval(Container.DataItem, "ixs1") %> </td>
+                                    <td><asp:LinkButton ID="LinkButtonVerFicheiro" runat="server" CommandName="ClickVerFicheiro" CommandArgument='<%# Eval("ip") %>' Text="Ver Ficheiro"></asp:LinkButton></td>
+                                </tr>
+                            </ItemTemplate>
+                            <FooterTemplate>
+                                </table>
+                            </FooterTemplate>
+                        </asp:Repeater>
+                    </div>
+                    <div>
+                        <asp:TextBox ID="TextBox2" runat="server" TextMode="MultiLine"></asp:TextBox>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonPesquisa_Click(object sender, EventArgs e)
+        {
+            LabelErros.Text = "";
+            LabelRegistos.Text = "";
+            LabelColuna.Text = "";
+            LabelIP.Text = "";
+            string chave = TextBoxPesquisa.Text;
+            if (string.IsNullOrEmpty(chave))
+            {
+                LabelErros.Text = "introduza dados";
+            }
+            else
+            {
+                chave = LimpaChave(chave);
+                if (chave.StartsWith("ixp")) PesquisaIXP(chave);
+                else PesquisaTodos(chave);
+            }
+        }
+
+        protected void Repeater1_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            if (e.CommandName == "ClickVerFicheiro")
+            {
+                string ip = e.CommandArgument.ToString();
+                LabelIP.Text = ip;
+                EscreveConteudoFicheiro(ip);
+            }
+        }
+
+        private void EscreveConteudoFicheiro(string ip)
+        {
+            ip = ip.Replace(".", "-");
+            ip = System.IO.File.ReadAllText(@"C:\Users\webrp\Downloads\" + ip + ".txt");
+            TextBox2.Text = ip;
+        }
+
+        private string LimpaChave(string chave)
+        {
+            chave = chave.Trim().ToLower();
+            while (chave.Contains(" "))
+            {
+                chave = chave.Replace(" ", string.Empty);
+            }
+            return chave;
+        }
+
+        private void PesquisaIXP(string chave)
+        {
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["teste-ptConnectionString"].ConnectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT * from xpto where ixp like '" + chave + "%'", connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    Repeater1.DataSource = reader;
+                    Repeater1.DataBind();
+                    if (Repeater1.Items.Count > 0)
+                    {
+                        LabelErros.Text = Repeater1.Items.Count.ToString();
+                    }
+                    else LabelRegistos.Text = "Sem dados";
+                    LabelColuna.Text = "IXP";
+                }
+                catch (Exception ex)
+                {
+                    LabelErros.Text = ex.Message.ToString();
+                }
+            }
+        }
+
+        private void PesquisaTodos(string chave)
+        {
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["teste-ptConnectionString"].ConnectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT * from xpto where ixp like '%" + chave + "%'", connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    Repeater1.DataSource = reader;
+                    Repeater1.DataBind();
+                    if (Repeater1.Items.Count > 0)
+                    {
+                        LabelRegistos.Text = Repeater1.Items.Count.ToString();
+                    }
+                    else LabelRegistos.Text = "Sem dados";
+                    LabelColuna.Text = "Todas";
+                }
+                catch (Exception ex)
+                {
+                    LabelErros.Text = ex.ToString();
+                }
+            }
+        }
+
+    }
+}
